1a9b121d1d1dc496f80ded834a9792ba1b471e75
Gregolly frança <42480355+gregolly@users.noreply.github.com>
2021-06-08 23:18:18 -0400

    Desafio 05

---
+++ src/Desafio.java
@@ -0,0 +1,25 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class Desafio {
+    public static void main(String[] args) {
+
+        Boolean verdadeiro = new Boolean(true);
+        Character blabla = new Character('c');
+        Byte numero1 = new Byte((byte) 120);
+        Short numeroCurto = new Short((short) 1.5);
+        Integer numero = new Integer(1);
+        Long longo = 2130213021032L;
+        Float flutuacao = new Float(31.5);
+        Double quebrado = new Double(23.5555);
+
+
+        System.out.println(verdadeiro);
+        System.out.println(blabla);
+        System.out.println(numero1);
+        System.out.println(numeroCurto);
+        System.out.println(numero);
+        System.out.println(longo);
+        System.out.println(flutuacao);
+        System.out.println(quebrado);
+    }
+}
